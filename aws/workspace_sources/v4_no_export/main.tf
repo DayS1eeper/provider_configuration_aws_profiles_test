cbe432c59ff47306b3a3f8b1b48d6473dd00e2e4
Vladyslav Mihun <mihunvlad@gmail.com>
2022-08-24 16:47:49 +0300

    add v4_no_export workspace

---
+++ aws/workspace_sources/v4_no_export/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.0.0"
+    }
+  }
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+# for aws_service entity type
+provider "aws" {
+  alias  = "aws_service"
+  region = "us-east-1"
+}
+
+variable "object_aws_service" {
+  type = string
+}
+
+resource "aws_s3_bucket_object" "aws_service" {
+  provider = aws.aws_service
+  bucket   = var.bucket_name
+  key      = var.object_aws_service
+  content  = "object_aws_service content"
+}
+
+# for aws_account entity type
+provider "aws" {
+  alias  = "aws_account"
+  region = "us-east-1"
+}
+
+variable "object_aws_account" {
+  type = string
+}
+
+resource "aws_s3_bucket_object" "aws_account" {
+  provider = aws.aws_account
+  bucket   = var.bucket_name
+  key      = var.object_aws_account
+  content  = "object_aws_account content"
+}
